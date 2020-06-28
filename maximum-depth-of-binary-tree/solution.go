bfd60d3f5245072784c96264c37fd1ff7ba69c3a
nsega <n-sega@mercari.com>
2020-06-29 03:05:56 +0900

    add(solution): add the solution for 104. Maximum Depth of Binary Tree

---
+++ maximum-depth-of-binary-tree/solution.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BinaryTree struct {
+	root *TreeNode
+}
+
+func (t *BinaryTree) insert(val int) *BinaryTree {
+	if t.root == nil {
+		t.root = &TreeNode{Val: val, Left: nil, Right: nil}
+	} else {
+		t.root.insert(val)
+	}
+	return t
+}
+
+func (n *TreeNode) insert(val int) {
+	if n == nil {
+		return
+	} else if val <= n.Val {
+		if n.Left == nil {
+			n.Left = &TreeNode{Val: val, Left: nil, Right: nil}
+		} else {
+			n.Left.insert(val)
+		}
+	} else {
+		if n.Right == nil {
+			n.Right = &TreeNode{Val: val, Left: nil, Right: nil}
+		} else {
+			n.Right.insert(val)
+		}
+	}
+}
+
+func print(node *TreeNode, ns int, ch rune) {
+	if node == nil {
+		return
+	}
+
+	for i := 0; i < ns; i++ {
+		fmt.Printf(" ")
+	}
+	fmt.Printf("%c:%v\n", ch, node.Val)
+	print(node.Left, ns+2, 'L')
+	print(node.Right, ns+2, 'R')
+}
+
+func maxDepth(root *TreeNode) int {
+
+	if root == nil {
+		return 0
+	}
+
+	// breadth first search of the whole tree.
+	que := []*TreeNode{root}
+	depth := 1
+	start, end := 0, 1
+
+	for {
+		numOfChildren := 0
+		for i := start; i < end; i++ {
+			node := que[i]
+
+			if node.Left != nil {
+				que = append(que, node.Left)
+				numOfChildren++
+			}
+
+			if node.Right != nil {
+				que = append(que, node.Right)
+				numOfChildren++
+			}
+		}
+		if numOfChildren == 0 {
+			break
+		}
+
+		depth++
+		start = end
+		end = end + numOfChildren
+	}
+	return depth
+}
+
+func main() {
+
+	tree := &BinaryTree{}
+	tree.insert(9).insert(3).insert(20).insert(15).insert(7)
+	fmt.Println(maxDepth(tree.root))
+	print(tree.root, 0, 'M')
+}
