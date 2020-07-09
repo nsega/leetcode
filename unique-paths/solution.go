924e1e19b648ba37bcbc3db533d14d8e6ad65a8c
nsega <n-sega@mercari.com>
2020-07-10 02:42:52 +0900

    add(solution): add the solution for 62. Unique Paths

--- unique-paths/solution.go
+++ unique-paths/solution.go
@@ -3,21 +3,21 @@ package main
 import "fmt"
 
 func uniquePaths(m int, n int) int {
-	
+
 	res := make([][]int, m)
-	for i:= 0; i<m; i++ {
+	for i := 0; i < m; i++ {
 		res[i] = make([]int, n)
 	}
 
-	for i:=0; i<m; i++ {
+	for i := 0; i < m; i++ {
 		res[i][0] = 1
 	}
-	for j:=0; j<n; j++ {
+	for j := 0; j < n; j++ {
 		res[0][j] = 1
 	}
 
-	for i:=1; i<m; i++ {
-		for j:=1; j<n; j++ {
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
 			res[i][j] = res[i-1][j] + res[i][j-1]
 		}
 	}
@@ -26,10 +26,10 @@ func uniquePaths(m int, n int) int {
 
 func main() {
 	m := 3
-	n :=2
-	fmt.Println("Output:", uniquePaths(m,n))
+	n := 2
+	fmt.Println("Output:", uniquePaths(m, n))
 
 	m = 7
 	n = 3
-	fmt.Println("Output:", uniquePaths(m,n))
+	fmt.Println("Output:", uniquePaths(m, n))
 }
