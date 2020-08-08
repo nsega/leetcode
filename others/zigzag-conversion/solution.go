77805ce80aaac9dc4c1aebb26d80f085c1c361ac
nsega <n-sega@mercari.com>
2020-08-09 01:49:30 +0900

    feat: add the solution of the problem 6. ZigZag Conversion

--- others/zigzag-conversion/solution.go
+++ others/zigzag-conversion/solution.go
@@ -26,6 +26,28 @@ func convert(s string, numRows int) string {
 	}
 	n := len(s)
 	runes := []rune(s)
+	runesRet := make([]rune, n)
+	k := 0
+	cycleLen := 2*numRows - 2
+	for i := 0; i < numRows; i++ {
+		for j := 0; j+i < n; j += cycleLen {
+			runesRet[k] = runes[j+i]
+			k++
+			if i != 0 && i != numRows-1 && j+cycleLen-i < n {
+				runesRet[k] = runes[j+cycleLen-i]
+				k++
+			}
+		}
+	}
+	return string(runesRet)
+}
+
+func convert2(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	n := len(s)
+	runes := []rune(s)
 	runesOut := make([]rune, n)
 	j := 0
 	for row := 0; row < numRows; row++ {
