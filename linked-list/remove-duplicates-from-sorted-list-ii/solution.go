d97400f2ddb5bd35e66ff6752c31068f81e21f53
nsega <n-sega@mercari.com>
2020-10-09 02:28:16 +0900

    refactor(solution): refactor the solution of 82. Remove Duplicates from Sorted List II

--- linked-list/remove-duplicates-from-sorted-list-ii/solution.go
+++ linked-list/remove-duplicates-from-sorted-list-ii/solution.go
@@ -1,3 +1,69 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	vals := []int{2, 3, 3, 4, 4, 5}
+	var l ListNode
+	l = ListNode{Val: 1, Next: nil}
+	for _, v := range vals {
+		l.Append(v)
+	}
+	fmt.Println("Example 1:")
+	fmt.Printf("  Input: ")
+	printListNode(&l)
+	fmt.Println("  Expected output:  1->2->5")
+	out := deleteDuplicates(&l)
+	fmt.Printf("  Actual output:  ")
+	printListNode(out)
+
+	vals = []int{1, 1, 2, 3}
+	l = ListNode{Val: 1, Next: nil}
+	for _, v := range vals {
+		l.Append(v)
+	}
+	fmt.Println("Example 2:")
+	fmt.Printf("  Input: ")
+	printListNode(&l)
+	fmt.Println("  Expected output:  2->3")
+	out = deleteDuplicates(&l)
+	fmt.Printf("  Actual output:  ")
+	printListNode(out)
+}
+
+// Append adds an Item to the end of the linked list
+func (l *ListNode) Append(v int) {
+	node := ListNode{Val: v, Next: nil}
+	if l.Next == nil {
+		l.Next = &node
+	} else {
+		last := l.Next
+		for {
+			if last.Next == nil {
+				break
+			}
+			last = last.Next
+		}
+		last.Next = &node
+	}
+}
+
+func printListNode(in *ListNode) {
+	for in != nil {
+		fmt.Printf("%d", in.Val)
+		if in.Next != nil {
+			fmt.Printf("->")
+		}
+		in = in.Next
+	}
+	fmt.Printf("\n")
+}
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -5,32 +71,29 @@
  *     Next *ListNode
  * }
  */
- func deleteDuplicates(head *ListNode) *ListNode {
-    
-    previous := head
-    current := head
-        
-    for previous != nil && previous.Next != nil {
-        
-        if current != nil && current.Next != nil && current.Next.Val == current.Val {
-            
-            for current != nil && current.Next != nil && current.Next.Val == current.Val {
-                current.Next = current.Next.Next                                
-            }
-
-            if current == head {
-                head = head.Next
-                previous = head
-                current = head
-            } else {
-                current = current.Next
-                previous.Next = current
-            }            
-        } else {
-            previous = current
-            current = current.Next
-        }
-    }
-    return head
-    
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	var temp *ListNode
+	for current != nil {
+		if current.Next != nil && current.Val == current.Next.Val {
+			if temp != nil {
+				for current.Next != nil && current.Val == current.Next.Val {
+					current = current.Next
+				}
+				temp.Next = current.Next
+				current = current.Next
+				continue
+			} else {
+				for current.Next != nil && current.Val == current.Next.Val {
+					current = current.Next
+				}
+				head = current.Next
+				current = current.Next
+				continue
+			}
+		}
+		temp = current
+		current = current.Next
+	}
+	return head
 }
