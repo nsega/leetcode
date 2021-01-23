671c4f446bb62bba6e9aadcd8b9895666bed10db
Naoki Sega <nsegaster@gmail.com>
2021-01-23 16:41:00 +0900

    chore: add package name and import statements

--- hash-table/unique_email_address/solution.go
+++ hash-table/unique_email_address/solution.go
@@ -1,12 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
 func numUniqueEmails(emails []string) int {
-    
-    m := make(map[string]int)    
-    for _, email := range emails {
-        s := strings.Split(email, "@")
-        local := strings.Split(s[0],"+")        
-        l := strings.ReplaceAll(local[0], ".", "")            
-        email = fmt.Sprintf("%s@%s", l,s[1])
-        m[email]++
-    }
-    return len(m)
+
+	m := make(map[string]int)
+	for _, email := range emails {
+		s := strings.Split(email, "@")
+		local := strings.Split(s[0], "+")
+		l := strings.ReplaceAll(local[0], ".", "")
+		email = fmt.Sprintf("%s@%s", l, s[1])
+		m[email]++
+	}
+	return len(m)
 }
